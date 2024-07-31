cc086037f9811435304e6d27c89c9ed8fe5a7545
ankush-anonymous <canankushdoit@gmai.com>
2024-08-01 00:50:05 +0530

    new commit

--- src/constants/componentProperties.js
+++
@@ -1,7 +0,0 @@
-// src/componentProperties.js
-export const initialProperties = {
-  Battery: { type: "Lion", voltageRating: "24v", capacity: "100Ah" },
-  Fan: { type: "DC", power: "36w" },
-  Bulb: { type: "DC", power: "24w" },
-  Switch: { type: "toggle" }, // Example property for switch
-};
